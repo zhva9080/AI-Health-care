6e692e0dd7b405f8ae7a0cb00912bf421ba7a3b8
zhva9080 <zhva9080@gmail.com>
2024-10-02 15:48:24 +0530

    changes in store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import CounterSlice from '../Modules/User/slices/CounterSlice'
 import LoginSlice from '../Modules/admin/slices/LoginSlice'
 import  patientSlice from '../Modules/User/slices/PatientSlice'
 import userRegisterSlice from '../Modules/User/slices/RegisterSlice'
@@ -8,12 +7,11 @@ import patientHistorySlice  from '../Modules/User/slices/HistorySlice'
 
 export const store = configureStore({
   reducer: {
-    counter:CounterSlice,
+    
     login:LoginSlice,
     patientdetails:patientSlice,
     userRegister:userRegisterSlice,
     userlogin:Userloginslice,
     userhistory:patientHistorySlice
-
   },
 })
